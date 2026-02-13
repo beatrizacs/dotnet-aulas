ae9d7b6a6e4d5fb502c0940f436b0027d4670161
Ana Silva <ana.beatriz.kjj@gmail.com>
2026-02-13 20:34:00 -0300

    Aula 22 - Lista de bolos (com classe)

---
+++ aulas/Aula26/Cake.cs
@@ -0,0 +1,10 @@
+namespace Aula26
+{
+    public class Cake
+    {
+       public Cake()
+       {
+        Console.WriteLine("Construtor!");
+       }
+    }
+}
---
+++ aulas/Aula26/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula26;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Instância da Classe
+        Cake cake = new Cake();
+
+        //Console.WriteLine(cake);
+    }
+}
