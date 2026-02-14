9b2724405e7d40cfca90f028ce4b53de27b9662a
Ana Silva <ana.beatriz.kjj@gmail.com>
2026-02-13 21:46:38 -0300

    Aula 26 - Construtores 1

--- aulas/Aula26/Cake.cs
+++ aulas/Aula26/Cake.cs
@@ -2,9 +2,21 @@ namespace Aula26
 {
     public class Cake
     {
-       public Cake()
+
+        public string Flavor;
+        public string Filling;
+        public string Coverage;
+        public int Layers;
+        public string Size;
+
+       public Cake()  // Inicializar uma instancia de uma classe (ctor)
        {
-        Console.WriteLine("Construtor!");
+            Flavor = "Chocolate";
+            Filling = "Coco";
+            Coverage = "Chocolate";
+            Layers = 3;
+            Size = "Médio";
        }
+
     }
 }
--- aulas/Aula26/Program.cs
+++ aulas/Aula26/Program.cs
@@ -5,7 +5,14 @@ public class Program
     public static void Main()
     {
         //Instância da Classe
-        Cake cake = new Cake();
+        Cake cake = new Cake(); // sempre vai ser executado oq está lá
+
+        Console.WriteLine($"Sabor: {cake.Flavor} ");
+        Console.WriteLine($"Recheio: {cake.Filling} ");
+        Console.WriteLine($"Cobertura: {cake.Coverage} ");
+        Console.WriteLine($"Camadas: {cake.Layers} ");
+        Console.WriteLine($"Tamanho: {cake.Size} ");
+        
 
         //Console.WriteLine(cake);
     }
