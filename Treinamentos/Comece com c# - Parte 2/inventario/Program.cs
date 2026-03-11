f30da803d1c7eb9f59626f26554d046e737a73d7
Ana Silva <ana.beatriz.kjj@gmail.com>
2026-03-11 16:43:43 -0300

    Foreach

---
+++ Treinamentos/Comece com c# - Parte 2/inventario/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] inventario = { 200, 450, 700, 175, 250 };
+int soma = 0;
+int caixa = 0;
+
+foreach (int itens in inventario)
+{
+    soma += itens;
+    caixa++;
+    Console.WriteLine($"caixa {caixa} = {itens} itens (Total acumulado: {soma})");
+}
+
+Console.WriteLine($"\nNós temos {soma} itens em estoque.");
