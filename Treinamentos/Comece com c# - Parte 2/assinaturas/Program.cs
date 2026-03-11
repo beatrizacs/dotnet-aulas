2e3e6433e7c28fe71a5dd8b15bd9e530dd59895c
Ana Silva <ana.beatriz.kjj@gmail.com>
2026-03-11 11:51:25 -0300

    Radom + if/else básico

---
+++ Treinamentos/Comece com c# - Parte 2/assinaturas/Program.cs
@@ -0,0 +1,27 @@
+﻿Random random = new Random();
+int diasParaVencimento = random.Next(12);
+int descontoPercentual = 0;
+
+if (diasParaVencimento == 0)
+{
+    Console.WriteLine("Sua assinatura expirou.");
+}
+else if (diasParaVencimento == 1)
+{
+    Console.WriteLine("Sua assinatura expira em um dia!");
+    descontoPercentual = 20;
+}
+else if (diasParaVencimento <= 5)
+{
+    Console.WriteLine($"Sua assinatura expira em {diasParaVencimento} days.");
+    descontoPercentual = 10;
+}
+else if (diasParaVencimento <= 10)
+{
+    Console.WriteLine("Sua assinatura expirará em breve. Renove agora!");
+}
+
+if (descontoPercentual > 0)
+{
+    Console.WriteLine($"Renove agora e economize {descontoPercentual}%.");
+} 
