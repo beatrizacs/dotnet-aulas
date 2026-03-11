ae690f9a049e75f11dc081efa9f0c656c0d8c7f9
Ana Silva <ana.beatriz.kjj@gmail.com>
2026-03-11 11:53:54 -0300

    string[]

---
+++ Treinamentos/Comece com c# - Parte 2/encomendasFraudulentas/Program.cs
@@ -0,0 +1,17 @@
+﻿//string[] IDsEncomendasFraudulentas = new string[3];
+
+//IDsEncomendasFraudulentas[0] = "A123";
+//IDsEncomendasFraudulentas[1] = "B123";
+//IDsEncomendasFraudulentas[2] = "C123";
+//IDsEncomendasFraudulentas[3] = "D000";
+
+string[] IDsEncomendasFraudulentas = { "A123", "B456", "C789" };
+
+Console.WriteLine($"Primeiro: {IDsEncomendasFraudulentas[0]}");
+Console.WriteLine($"Segundo: {IDsEncomendasFraudulentas[1]}");
+Console.WriteLine($"Terceiro: {IDsEncomendasFraudulentas[2]}");
+
+IDsEncomendasFraudulentas[0] = "F000"; // teste
+
+Console.WriteLine($"Atribuir primeiro: {IDsEncomendasFraudulentas[0]}");
+Console.WriteLine($"Existem {IDsEncomendasFraudulentas.Length} encomendas fraudulentas para processar.");
